c3f9e88039eb853b520a033f315777833b9f9888
Kimmo Koskinen <kimmo@tiuhti.net>
2015-07-29 20:21:00 +0300

    Login when pressing enter in password field

--- src/cljs/shopping_list/components/login.cljs
+++ src/cljs/shopping_list/components/login.cljs
@@ -3,6 +3,17 @@
             [ajax.core :refer [POST]]
             [shopping-list.state :as state]))
 
+(defn login [username password]
+  (POST "/login"
+      {:params {:username @username
+                :password @password}
+       :handler (fn [items]
+                  (reset! username nil)
+                  (reset! password nil)
+                  (state/set-view :items items))
+       :error-handler (fn [{:keys [status status-text failure]}]
+                        (.log js/console (str status status-text failure)))}))
+
 (defn login-view []
   (let [username (reagent/atom nil)
         password (reagent/atom nil)]
@@ -16,18 +27,12 @@
        [:diw.row
         [:span.column.label "Salasana"]
         [:input.column {:type "password"
+                        :on-key-down (fn [event]
+                                       (when (= 13 (.-keyCode event))
+                                         (login username password)))
                         :on-change (fn [event]
                                      (reset! password (-> event .-target .-value)))}]]
        [:div.row
         [:span.column] [:button.column
-                        {:on-click (fn [_]
-                                     (POST "/login"
-                                         {:params {:username @username
-                                                   :password @password}
-                                          :handler (fn [items]
-                                                     (reset! username nil)
-                                                     (reset! password nil)
-                                                     (state/set-view :items items))
-                                          :error-handler (fn [{:keys [status status-text failure]}]
-                                                           (.log js/console (str status status-text failure)))}))}
+                        {:on-click #(login username password)}
                         "Kirjaudu"]]])))
