ecbb0d1138e1fae15a04516bd57b97257ebc8adc
Kimmo Koskinen <kimmo@tiuhti.net>
2015-07-30 22:50:56 +0300

    Add locale sensitive sorting

--- src/clj/shopping_list/endpoint/items.clj
+++ src/clj/shopping_list/endpoint/items.clj
@@ -1,19 +1,22 @@
 (ns shopping-list.endpoint.items
+  (:import [java.text Collator])
   (:require [compojure.core :refer [GET POST routes wrap-routes]]
             [ring.util.response :refer [response]]
             [ring.middleware.transit :refer [wrap-transit-response wrap-transit-body]]
             [shopping-list.middleware.session-timeout :refer [wrap-session-timeout]]
             [datomic.api :as d]))
 
+(def collator (Collator/getInstance (java.util.Locale/forLanguageTag "fi_FI")))
+
 (defn get-items [db]
-  (sort-by :name (for [[id name count] (d/q '[:find ?e ?name ?count
-                                              :where
-                                              [?e :item/name ?name]
-                                              [?e :item/count ?count]]
-                                            db)]
-                   {:id id
-                    :name name
-                    :count count})))
+  (sort-by :name collator (for [[id name count] (d/q '[:find ?e ?name ?count
+                                                       :where
+                                                       [?e :item/name ?name]
+                                                       [?e :item/count ?count]]
+                                                     db)]
+                            {:id id
+                             :name name
+                             :count count})))
 
 (defn items [session-timeout-secs {{conn :conn} :datomic}]
   (-> (routes
