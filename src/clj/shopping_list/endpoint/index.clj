9825dd0078efee2f118a405aa90336b5717a863f
Kimmo Koskinen <kimmo@tiuhti.net>
2015-08-02 19:16:54 +0300

    Serve index instead of redirect

--- src/clj/shopping_list/endpoint/index.clj
+++ src/clj/shopping_list/endpoint/index.clj
@@ -1,9 +1,10 @@
 (ns shopping-list.endpoint.index
   (:require [compojure.core :refer [GET routes]]
-            [ring.util.response :refer [redirect]]
+            [ring.util.response :refer [resource-response content-type]]
             [compojure.route :as route]))
 
 (defn index [_]
   (routes
    (GET "/" []
-     (redirect "/index.html"))))
+     (content-type (resource-response "index.html" {:root "public"})
+                   "text/html"))))
